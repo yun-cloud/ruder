3020810befe9f15731bab66506cdf7a5fdf0bc95
chchang <ookk8282@gmail.com>
2020-12-02 16:23:38 +0800

    [API] get asset download filename

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,6 @@
 use log::info;
 use query_the_github_api::github::query_latest_release;
+use skip_error::skip_error;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -7,6 +8,16 @@ async fn main() -> anyhow::Result<()> {
 
     let latest_release = query_latest_release("BurntSushi", "ripgrep").await?;
     info!("latest_release: {:#?}", latest_release);
+
+    for asset in latest_release.assets {
+        let filepath = skip_error!(asset.download_filename());
+        info!(
+            "filepath: {:?}, {:?}, {:?}",
+            filepath,
+            filepath.file_stem(),
+            filepath.extension()
+        );
+    }
 
     Ok(())
 }
--- src/github.rs
+++ src/github.rs
@@ -1,33 +1,55 @@
+use anyhow::anyhow;
 use reqwest::header;
 use reqwest::header::HeaderValue;
 use reqwest::Client;
 use serde::Deserialize;
+use std::path::PathBuf;
 use url::Url;
 
-#[derive(Debug, Deserialize)]
-pub struct Release {
-    tag_name: String,
-    name: String,
-    assets: Vec<Asset>,
-    body: String,
-    author: Author,
+macro_rules! pub_fields {
+    (
+        $(#[$deri:meta])*
+             struct $name:ident {
+                 $($field:ident: $t:ty,)*
+             }
+    ) => {
+        $(#[$deri])*
+        pub struct $name {
+            $(pub $field: $t),*
+        }
+    }
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Asset {
-    url: String,
-    name: String,
-    content_type: String,
-    state: String,
-    size: usize,
-    created_at: String,
-    updated_at: String,
-    browser_download_url: String,
+pub_fields! {
+    #[derive(Debug, Deserialize)]
+    struct Release {
+        tag_name: String,
+        name: String,
+        assets: Vec<Asset>,
+        body: String,
+        author: Author,
+    }
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Author {
-    login: String,
+pub_fields! {
+    #[derive(Debug, Deserialize)]
+    struct Asset {
+        url: String,
+        name: String,
+        content_type: String,
+        state: String,
+        size: usize,
+        created_at: String,
+        updated_at: String,
+        browser_download_url: String,
+    }
+}
+
+pub_fields! {
+    #[derive(Debug, Deserialize)]
+    struct Author {
+        login: String,
+    }
 }
 
 pub async fn query_latest_release(owner: &str, repo: &str) -> anyhow::Result<Release> {
@@ -39,17 +61,31 @@ pub async fn query_latest_release(owner: &str, repo: &str) -> anyhow::Result<Rel
 
     let mut headers = header::HeaderMap::new();
     headers.insert(header::USER_AGENT, HeaderValue::from_static("reqwest_try"));
+
     let client = Client::builder().default_headers(headers).build()?;
 
     let response = client
         .get(request_url.as_str())
         .header(header::ACCEPT, "application/vnd.github.v3+json")
-        .query(&[("per_page", "5")])
-        .query(&[("page", "2")])
         .send()
         .await?;
 
     let latest_release: Release = response.json().await?;
 
     Ok(latest_release)
+}
+
+impl Asset {
+    pub fn download_filename(&self) -> anyhow::Result<PathBuf> {
+        let browser_download_url = &self.browser_download_url;
+
+        let url = Url::parse(browser_download_url)?;
+        let filename = url
+            .path_segments()
+            .ok_or_else(|| anyhow!("browser_download_url no path segments"))?
+            .last()
+            .unwrap();
+
+        Ok(PathBuf::from(filename))
+    }
 }
